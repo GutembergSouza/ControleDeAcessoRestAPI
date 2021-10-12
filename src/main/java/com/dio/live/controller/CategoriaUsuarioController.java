acd9003a527940c18653072ccd002e4b8499d37a
GutembergSiqueira96 <gutembergsiqueira96@gmail.com>
2021-10-12 09:42:24 -0300

    desenvolvimento completo do crud referente a Categoria de usuario

--- src/main/java/com/dio/live/controller/CategoriaUsuarioController.java
+++ src/main/java/com/dio/live/controller/CategoriaUsuarioController.java
@@ -1,9 +1,43 @@
 package com.dio.live.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dio.live.model.CategoriaUsuario;
+import com.dio.live.repository.CategoriaUsuarioRepository;
+import com.dio.live.service.CategoriaUsuarioService;
+import com.dio.live.service.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categoriausuario")
 public class CategoriaUsuarioController {
+    @Autowired
+    CategoriaUsuarioService service;
+
+    @GetMapping
+    public List<CategoriaUsuario> listAll(){
+        return service.listAll();
+    }
+
+    @PostMapping
+    public CategoriaUsuario create(@RequestBody CategoriaUsuario categoriaUsuario){
+       return service.create(categoriaUsuario);
+    }
+
+    @GetMapping("/{id}")
+    public CategoriaUsuario findById(@PathVariable("id") Long id) throws NotFoundException {
+        return service.findById(id);
+    }
+
+    @PutMapping
+    public CategoriaUsuario update(@RequestBody CategoriaUsuario categoriaUsuario) throws NotFoundException {
+        return service.update(categoriaUsuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) throws NotFoundException {
+        service.delete(id);
+    }
+
 }
